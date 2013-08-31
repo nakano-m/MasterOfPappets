e9e9e9962a902dafa7b25d4bc1e0df2360b48fc1
qTanakaSatomip <s-tanaka@applicative.co.jp>
2013-08-31 14:46:55 +0900

    socket.ioの機能いろいろ追加

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var chatroom = require('./routes/myroom');
 var http = require('http');
 var path = require('path');
 
-var app = express();
+var app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -23,15 +24,39 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/static', express.static(__dirname + 'node_modules'));
 
-// development only
+// deploy env
 if ('development' == app.get('env')) {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+}
+
+if ('production' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Routes
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/myroom', chatroom.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+// application layout
+app.set('view_options', {
+    layout: 'layout'
+});
+
+// socket.io settings.
+io.sockets.on('connection', function (socket) {
+    console.log('connected(server side).');
+    socket.on('msg send', function (message) {
+        console.log('in msg send. message:' + message);
+        socket.emit('msg push', message);
+        socket.broadcast.emit('msg push', message);
+    });
+    socket.on('disconnect', function() {
+        console.log('disconnect');
+    });
+});
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ public/javascripts/chat.js
@@ -0,0 +1,24 @@
+(function($) {
+    
+    var socket = io.connect('http://localhost');
+    socket.on('connect', function() {
+        console.log('connected(client side)');
+    });
+
+    $(document).on('click', 'button#postbutton', function(e) {
+        var message = $('#message').val();
+        console.log("msg1 : " + message);
+        socket.emit('msg send', message);
+    });
+
+    socket.on('msg push', function(message) {
+        console.log("msg2 : " + message);
+        var date = new Date();
+        $('#list').prepend($('<dt>' + date + '</dt><dd>' + message + '</dd>'));
+    });
+
+    socket.on('msg updateDB', function(message) {
+        console.log("msg3 : " + message); 
+    });
+
+})(jQuery);
